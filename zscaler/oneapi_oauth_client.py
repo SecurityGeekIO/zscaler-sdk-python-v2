9636e2d4c48b1966796b73f1decf78d652648ec1
William Guilherme <wguilherme@securitygeek.io>
2025-03-10 21:57:48 -0700

    fix: oneapi_oauth_client.py

--- zscaler/oneapi_oauth_client.py
+++ zscaler/oneapi_oauth_client.py
@@ -105,65 +105,6 @@ class OAuth:
         # logging.debug("Authentication with client secret successful.")
         return response
 
-    # def _authenticate_with_private_key(self, client_id, private_key_path):
-    #     """
-    #     Authenticate using client ID and JWT private key.
-
-    #     Args:
-    #         client_id (str): Client ID for authentication.
-    #         private_key_path (str): Path to the private key file.
-
-    #     Returns:
-    #         str: OAuth access token.
-    #     """
-    #     logging.debug("Preparing to authenticate with JWT private key.")
-    #     vanity_domain = self._config["client"]["vanityDomain"]
-    #     cloud = self._config["client"].get("cloud", "PRODUCTION").lower()
-    #     auth_url = self._get_auth_url(vanity_domain, cloud)
-
-    #     # Read and load the private key
-    #     with open(private_key_path, "rb") as key_file:
-    #         private_key = serialization.load_pem_private_key(key_file.read(), password=None, backend=default_backend())
-
-    #     # Create JWT for client assertion
-    #     now = int(time.time())
-    #     payload = {
-    #         "iss": client_id,
-    #         "sub": client_id,
-    #         "aud": "https://api.zscaler.com",
-    #         "exp": now + 600,  # 10 minutes expiration
-    #     }
-
-    #     # Generate the assertion using RS256 algorithm
-    #     assertion = jwt.encode(payload, private_key, algorithm="RS256")
-
-    #     # Prepare form data
-    #     form_data = {
-    #         "grant_type": "client_credentials",
-    #         "client_id": client_id,
-    #         "client_assertion": assertion,
-    #         "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-    #         "audience": "https://api.zscaler.com",
-    #     }
-
-    #     user_agent = UserAgent().get_user_agent_string()
-    #     headers = {
-    #         "Accept": "application/json",
-    #         "Content-Type": "application/x-www-form-urlencoded",
-    #         "User-Agent": user_agent,
-    #     }
-
-    #     logging.debug(f"Sending authentication request to {auth_url} with JWT.")
-    #     # Synchronous HTTP request
-    #     response = requests.post(auth_url, data=form_data, headers=headers)
-
-    #     if response.status_code >= 300:
-    #         logging.error(f"Error authenticating: {response.status_code}, {response.text}")
-    #         raise Exception(f"Error authenticating: {response.status_code}, {response.text}")
-
-    #     logging.info("Authentication with JWT private key successful.")
-    #     return response
-
     def _authenticate_with_private_key(self, client_id, private_key):
         """
         Authenticate using client ID and JWT private key.
